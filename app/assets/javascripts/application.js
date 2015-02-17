7b2d0f45a9990e1446fd0b94be6f3f220c85fbe9
Chris Barnes <contact@barnesian.com>
2015-02-16 19:21:04 -0500

    Initialize empty ember-rails project.

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,11 @@
+//= require jquery
+//= require jquery_ujs
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require ./cars
+
+// for more details see: http://emberjs.com/guides/application/
+Cars = Ember.Application.create();
+
---
+++ app/assets/javascripts/router.js
@@ -0,0 +1,5 @@
+// For more information see: http://emberjs.com/guides/routing/
+
+Cars.Router.map(function() {
+  // this.resource('posts');
+});
